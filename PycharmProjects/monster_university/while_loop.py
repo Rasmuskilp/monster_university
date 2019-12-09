cf817a68cc5647f622b8011b5f4565c16c5f969b
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-09 12:14:04 +0000

    added the changes

--- PycharmProjects/monster_university/while_loop.py
+++ PycharmProjects/monster_university/while_loop.py
@@ -1,14 +1,32 @@
 from monster_workshop import *
 from monster_students import *
 from monster_lecture_theathre import *
+def clerk_functions():
+    # add_workshop = input('Enter the workshop:')
+    # Monster_workshop.workshop_list.append(add_workshop)
+    add_workshop_id = input('What is the workshops id?')
+    add_location = input('what is the location?')
+    add_staff = input('who is the professor?')
+    add_subject = input('What do they study?')
+    add_workshop_true = Monster_workshop(add_workshop_id, add_location, add_staff, add_subject, 0)
+    # add_name = input('what is the name of the student?')
+    # add_student = input('Enter the student_id:')
+    # add_skills = input('What is their skill?')
+    # add_grade = input('What is their grade')
+    # # Monster_students.add_grade(add_name,add_name,add_grade)
+    # add_name = Monster_students(f'{add_name}', add_skills, add_student, add_grade)
+    # Monster_workshop.add_student_workshop().append(add_student)
+    workshops.append(add_workshop_true)
+    # monsters.append(add_name)
 from run_uni import *
 while True:
     print('1 - create a monster')
     print('2 - list all workshops')
-    print('3 - add students to spooky workshop')
+    print('3 - add students to monster list')
     print('4 - see students grade')
     print('5 - print a students full information')
     print('6 - search for a student by name')
+    print('7 - add new workshop')
     interface = input('Enter the number for the option you wish to choose')
     if '1' in interface:
         add_student = input('what is the name of the student?')
@@ -34,6 +52,8 @@ while True:
                 break
             else:
                 print('error')
+    elif '7' in interface:
+            clerk_functions()
     else:
         print('Sorry but you did not enter a number, do you wish to exit, then press y')
         exits = input('press y if you want to exit')
